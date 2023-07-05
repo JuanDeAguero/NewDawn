20de17ea67b4843d0524e2700207313e6394ff4a
Juan <juanmgdeaguero@gmail.com>
2023-07-05 21:34:09 +0200

    Update GameModeND.h

--- Source/Game/GameModeND.h
+++ Source/Game/GameModeND.h
@@ -25,7 +25,8 @@ public:
     void Server_SpawnActor( TSubclassOf<AActorND> actorClass, const FLocation64& location, const FRotator& rotation );
 
     UFUNCTION( Server, Reliable )
-    void Server_SpawnPawn( TSubclassOf<APawnND> pawnClass, const FLocation64& location, const FRotator& rotation, class APlayerControllerND* controller );
+    void Server_SpawnPawn( TSubclassOf<APawnND> pawnClass, const FLocation64& location, const FRotator& rotation,
+                           class APlayerControllerND* controller );
 
 protected:
 
@@ -35,7 +36,8 @@ private:
 
     void Server_SpawnActor_Implementation( TSubclassOf<AActorND> actorClass, const FLocation64& location, const FRotator& rotation );
 
-    void Server_SpawnPawn_Implementation( TSubclassOf<APawnND> pawnClass, const FLocation64& location, const FRotator& rotation, class APlayerControllerND* controller );
+    void Server_SpawnPawn_Implementation( TSubclassOf<APawnND> pawnClass, const FLocation64& location, const FRotator& rotation, 
+                                          class APlayerControllerND* controller );
 
     int64 CreateActorId();
 
